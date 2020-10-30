b1eb2975514910e4de0b7aff1e2dabc4e5f20f30
jaga1990 <agnieszka@golofit.pl>
2020-10-30 15:33:42 +0100

    Creating new project: meeting_planner_aga

---
+++ meeting_planner_aga/meeting_planner_aga/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for meeting_planner_aga project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_planner_aga.settings')
+
+application = get_asgi_application()
