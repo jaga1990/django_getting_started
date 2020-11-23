cd7959e6a003d74e3cc574198449dc9c5e2524f7
jaga1990 <agnieszka@golofit.pl>
2020-11-23 11:05:28 +0100

    Adjusting link for room details

--- meeting_planner_aga/meetings/urls.py
+++ meeting_planner_aga/meetings/urls.py
@@ -3,6 +3,6 @@ from .views import detail, room_detail, rooms_list
 
 urlpatterns = [
     path('<int:id>', detail, name='detail'),
-    path('<int:id>', room_detail, name='room_detail'),
+    path('room/<int:id>', room_detail, name='room_detail'),
     path('roomslist', rooms_list, name='rooms_list'),
 ]
